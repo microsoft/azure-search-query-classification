20016e0369a37c0e7485e6ded06509d9a5db21dd
Vivien Hung <vivihung@microsoft.com>
2022-04-13 16:35:19 -0700

    fix escape error of path

--- local-run.py
+++ local-run.py
@@ -1,6 +1,10 @@
 from subprocess import Popen
 import os
-os.chdir(r'.\docker-api\code')
+
+api_code_path = os.path.join(os.path.curdir, "docker-api", "code")
+# abs_env_path = os.path.abspath(os.path.join(os.getcwd(), ".env"))
+os.chdir(api_code_path)
+
 commands = ["uvicorn --env-file ../../.env --port 8000 api:app", "python ../../check-api-load.py"]
 procs = [ Popen(i) for i in commands ]
 for p in procs:
