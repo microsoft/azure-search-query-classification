27c6a02aa9500a61744052e0e365645571516443
ruoccofabrizio <ruoccofabrizio@gmail.com>
2022-04-29 10:33:35 +0200

    fixed mac os execution

--- local-run.py
+++ local-run.py
@@ -1,7 +1,8 @@
 from subprocess import Popen
 import os
-os.chdir(r'.\docker-api\code')
-commands = ["uvicorn --env-file ../../.env --port 8000 api:app", "python ../../check-api-load.py"]
-procs = [ Popen(i) for i in commands ]
+api_code_path = os.path.join(os.path.curdir, "docker-api", "code")
+os.chdir(api_code_path)
+commands = [f"uvicorn --env-file {os.path.join('..','..','.env')} --port 8000 api:app", f"python {os.path.join('..','..','check-api-load.py')}"]
+procs = [ Popen(i, shell=True) for i in commands ]
 for p in procs:
    p.wait()
